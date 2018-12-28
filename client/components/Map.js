bb6a37f4abdbf15373564e20cc4b7eb126293ce3
Cody Jensen <jensen.cody.e@gmail.com>
2018-12-27 21:13:35 -0600

    first commit

---
+++ client/components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Map = () => {
+  return (
+    <table>
+      <tbody />
+    </table>
+  );
+};
+export default Map;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const PORT = 3000;
+const app = express();
+
+module.exports = app;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
